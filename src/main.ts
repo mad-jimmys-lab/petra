c06f2ce7cb5d5107a856c59ac4b405069863715c
tomoda-kazuya <tomoda1207.work@gmail.com>
2023-12-21 23:15:03 +0900

    fontsの追加（NotoSansJP）

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import router from './router'
 
 const pinia = createPinia()
