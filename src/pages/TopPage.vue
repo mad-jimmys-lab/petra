3c682706d3042cb9589d6c374019dc46ae4c8a16
tomoda-kazuya <tomoda1207.work@gmail.com>
2023-12-21 23:15:46 +0900

    Topページの実装

---
+++ src/pages/TopPage.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="h-screen">
+    <!-- TODO:logoもコンポーネント化したい、画像でよい気もしている -->
+    <div class="flex">
+      <img class="logo" src="../assets/images/bookIcon.svg" alt="logo">&nbsp;
+      <h1 class="title text-black">Petra<span class="sub-title">Liblary&nbsp;Manager</span></h1>
+    </div>
+    <!-- TODO:ボタンはコンポーネント化する -->
+    <button
+      class="mt-8 bg-black hover:bg-black_hover text-white font-bold py-3 px-24 rounded focus:outline-none focus:shadow-outline">Get
+      Started</button>
+  </section>
+</template>
+
+<style scoped lang="scss">
+// TODO:styleは共通化予定,fontsもどうにかしたい
+section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  max-width: 5rem;
+}
+
+.title {
+  font-family: 'NotoSansJP';
+  font-weight: bold;
+  font-size: 5rem;
+
+  .sub-title {
+    font-family: 'NotoSansJP';
+    font-weight: 300;
+    font-size: 2rem;
+    margin-left: 8px;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import TopPage from '../pages/TopPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    { 
+      path: '/',
+      name: 'TopPage',
+      component: TopPage 
+    },
+  ]
 })
 
 export default router
