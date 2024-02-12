acba538f4e1581d2c549776a4769ece6d542959c
*zgjDlete7F <k-tsuchiya@star-career.co.jp>
2024-02-12 18:39:00 +0900

    setting

--- src/pages/LoginTop.vue
+++ src/pages/LoginTop.vue
@@ -1,4 +1,41 @@
 <script setup lang="ts">
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const email = ref('');
+const password = ref('');
+const router = useRouter();
+const errMsg = ref('');
+const auth = getAuth();
+
+function login() {
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then(() => {
+      console.log(auth.currentUser);
+      router.push('/BookList');
+    })
+    .catch((error) => {
+      console.log(error.code)
+      switch (error.code) {
+        //メールアドレスが違うエラー
+        case "auth/invalid-email":
+          errMsg.value = "メールアドレスが不正です";
+          break;
+        //ユーザーが存在しないエラー
+        case "auth/user-not-found":
+          errMsg.value = "ユーザーが見つかりません";
+          break;
+        //パスワードが違うエラー
+        case "auth/wrong-password":
+          errMsg.value = "パスワードが間違っています";
+          break;
+      }
+    });
+}
 
 </script>
 
@@ -13,18 +50,19 @@
       <div class="mb-4">
         <input
           class="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          id="mail" type="text" placeholder="メールアドレス">
+          id="email" type="email" placeholder="メールアドレス" v-model="email">
       </div>
       <div class="mb-4">
         <input
           class="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          id="password" type="password" placeholder="パスワード">
+          id="password" type="password" placeholder="パスワード" v-model="password">
       </div>
+      <p v-if="errMsg">{{ errMsg }}</p>
     </div>
     <!-- TODO:ボタンはコンポーネント化する -->
     <button
       class="mt-8 bg-black hover:bg-black_hover text-white font-bold py-3 px-24 rounded focus:outline-none focus:shadow-outline"
-      @click="() => $router.push({ name: 'Home' })">Login</button>
+      @click="login">Login</button>
   </section>
 </template>
 
